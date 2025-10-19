723431564e66978780c151d21f564bb09e007d22
Ankit G <108504829+AnkitG791m@users.noreply.github.com>
2025-10-19 02:24:31 -0700

    Create Write a program to print the first 10 natural numbers in reverse order.c
    Write a program to print the first 10 natural numbers in reverse order

---
+++ assignment 4/Write a program to print the first 10 natural numbers in reverse order.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+// Write a program to print the first 10 natural numbers in reverse order
+
+
+	        
+	       
+	        for(int i=10; i>=0; i--){
+	            printf("%d\n",i);
+	        }
+}
