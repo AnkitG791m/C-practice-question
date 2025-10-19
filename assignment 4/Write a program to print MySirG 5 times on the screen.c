194b1a57730141340485afc963a7b60fa43805ad
Ankit G <108504829+AnkitG791m@users.noreply.github.com>
2025-10-19 02:18:45 -0700

    Add files via upload

---
+++ assignment 4/Write a program to print MySirG 5 times on the screen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+//Write a program to print MySirG 5 times on the screen
+
+	        
+	       
+	        for(int i=1; i<=5; i++){
+	            printf("Hey i am ankit \n");
+	        }
+}
