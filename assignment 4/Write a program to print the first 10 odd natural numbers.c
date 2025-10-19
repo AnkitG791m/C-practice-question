7215346f53f910466fee9f122d2a92d0770b0023
Ankit G <108504829+AnkitG791m@users.noreply.github.com>
2025-10-19 02:32:06 -0700

    Add program to print first 10 odd natural numbers
    This program prints the first 10 odd natural numbers.

---
+++ assignment 4/Write a program to print the first 10 odd natural numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+//  Write a program to print the first 10 odd natural numbers
+
+
+	        
+	       
+	        for(int i=1; i<20;i+=2){
+	            printf("%d\n",i);
+	          
+	        }
+}
